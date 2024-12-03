c45904155cd694601fac7c83cc5a3b3838a90a40
Dulaj Nawodya <dulajnawodya5@gmail.com>
2024-12-03 14:24:04 +0530

    www

--- pages/getUserDetails.tsx
+++ pages/getUserDetails.tsx
@@ -399,7 +399,7 @@ const UserDetails: NextPage<Props> = ({ dirs }) => {
                     <div className="conlainer-fluid m-0 px-3 px-lg-0 py-3 py-lg-5 my-3 my-lg-5 w-100 d-flex justify-content-center align-items-center">
                       <div className="d-flex flex-column justify-content-center align-items-center text-center mt-5 pt-5 transparent-select-box">
                         <div id="blur_background"></div>
-                        <h2 className="text-white font-36 ps-5 ms-5">
+                        <h2 className="text-white font-36 ps-lg-5 ms-lg-5">
                           ENTER YOUR DETAILS
                         </h2>
                         <form
