2be64dbadd08d2fc8ea587059477c05754c31785
Dulaj Nawodya <dulajnawodya5@gmail.com>
2024-12-04 16:06:38 +0530

    new home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,37 +44,46 @@ export default function Home() {
           <div className="home_slider_container p-0 m-0 position-relative">
             <div>
               <div className="home_slider_image_container image1 p-0 m-0">
-                <div className="d-flex justify-content-center align-items-end w-100 hero-container">
+                <div className="d-flex justify-content-center align-items-center w-100 hero-container" style={{minHeight:'100vh'}}>
                   <div className="d-flex flex-column justify-content-center align-items-center home-txt-container">
-                    <h1 className="text-center home-text px-2">YOU HAVE THE POWER TO CHANGE YOUR FUTURE WITH SEYLAN</h1>
                     
-                  </div>
-                </div>
-                <div className="d-flex justify-content-center align-items-center b-space w-100 sub-container">
-                  <div className="d-flex flex-column justify-content-center align-items-center home-txt-container">
+                    
+                  <Image
+                      src={'/Gen z can.png'}
+                      height={500}
+                      width={500}
+                      alt=''
+                      className='img-fluid genZ-img'
+                      
+                    />
                     <h2
                       className="text-center px-2 text-white"
                       style={{
                         fontWeight: 'bold',
                         fontSize: '20px',
-                        marginTop: '150px',
+                        // marginTop: '60px',
                         width: '67%',
                       }}
                     >
-                      At Seylan Bank&apos;s Motor Show, unleash your imagination! Share your
-                      dream vehicle idea, and our AI will transform it into a stunning visual.
-                      Experience innovation like never before, only with Seylan Bank.
+                     Live Your Life, Chase Your Dreams and Capture the moment
                     </h2>
                     <Link
                       href="/getUserDetails"
                       className="d-flex justify-content-center align-items-center"
                     >
-                      <button type="submit" className="mt-3 submit-btn-home">
+                      <button type="submit" className="mt-3 mb-5 submit-btn-home">
                         Next
                       </button>
                     </Link>
+                    
                   </div>
                 </div>
+                {/* <div className="d-flex justify-content-center align-items-center b-space w-100 sub-container">
+                  <div className="d-flex flex-column justify-content-center align-items-center home-txt-container">
+                  
+                    
+                  </div>
+                </div> */}
               </div>
             </div>
           </div>
--- pages/teaserPage.tsx
+++ pages/teaserPage.tsx
@@ -40,25 +40,33 @@ export default function Home() {
   return (
     <>
       <Layout>
-        <div className="container-fluid m-0 p-0 background_teaser">
+        <div className="container-fluid m-0 p-0 background_home">
           <div className="home_slider_container p-0 m-0 position-relative">
             <div>
               <div className="home_slider_image_container image1 p-0 m-0">
-                <div className="d-flex justify-content-center align-items-end w-100 hero-container">
+                <div className="d-flex justify-content-center align-items-center w-100 hero-container" style={{minHeight:'100vh'}}>
                   <div className="d-flex flex-column justify-content-center align-items-center home-txt-container">
                     
                     
+                  <Image
+                      src={'/Gen z can.png'}
+                      height={500}
+                      width={500}
+                      alt=''
+                      className='img-fluid'
+                      style={{
+                        verticalAlign:'center',
+                        height:'auto',
+                        width:'30vw'
+                      }}
                     
-                  </div>
-                </div>
-                <div className="d-flex justify-content-center align-items-center b-space w-100 sub-container">
-                  <div className="d-flex flex-column justify-content-center align-items-center home-txt-container">
+                    />
                     <h2
                       className="text-center px-2 text-white"
                       style={{
                         fontWeight: 'bold',
                         fontSize: '20px',
-                        marginTop: '150px',
+                        // marginTop: '60px',
                         width: '67%',
                       }}
                     >
@@ -72,14 +80,66 @@ export default function Home() {
                         Next
                       </button>
                     </Link>
+                    
                   </div>
                 </div>
+                {/* <div className="d-flex justify-content-center align-items-center b-space w-100 sub-container">
+                  <div className="d-flex flex-column justify-content-center align-items-center home-txt-container">
+                  
+                    
+                  </div>
+                </div> */}
               </div>
             </div>
           </div>
         </div>
 
-        
+        <div className="container-fluid m-0 background_slider p-0">
+          <div className="slider_container p-0 m-0 position-relative">
+            <Link
+              className="gallery_link"
+              href='/imageSwiper'
+              // href="https://sites.techvoice.lk/seylan-ai-backend/api/get-completed-images"
+            >
+              <h3>Image Swiper</h3>
+            </Link>
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              loop={true}
+              speed={3000}
+              autoplay={{
+                delay: 0,
+                disableOnInteraction: false,
+              }}
+              navigation
+              spaceBetween={0}
+              slidesPerView={4}
+              onSlideChange={() => console.log('slide change')}
+              onSwiper={(swiper) => console.log(swiper)}
+              breakpoints={{
+                320: { slidesPerView: 1, spaceBetween: 0 },
+                480: { slidesPerView: 1, spaceBetween: 0 },
+                640: { slidesPerView: 2, spaceBetween: 0 },
+                1024: { slidesPerView: 4, spaceBetween: 0 },
+                1336: { slidesPerView: 4, spaceBetween: 0 },
+              }}
+            >
+              {images.map((image) => (
+                <SwiperSlide key={image.id} style={{ padding: '10px' }}>
+                  <Link href="#">
+                    <Image
+                      src={image.image_url}
+                      alt={`Image ${image.id}`}
+                      width={250}
+                      height={250}
+                      className="img-fluid rounded-3"
+                    />
+                  </Link>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
       </Layout>
     </>
   );
